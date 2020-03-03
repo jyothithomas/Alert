ddaac6a9da594bb5826bb3de03244c5c49288420
jyothi thomas <jyothithomas.05@gmail.com>
2020-03-02 21:21:56 -0500

    [jyothi]

--- Alert/SecondViewController.swift
+++ Alert/SecondViewController.swift
@@ -9,16 +9,50 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var imgCircus: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //imgCircus.image = UIImage(named: "lion.png")
+       // imgCircus.backgroundColor = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)
     }
     
 
+    @IBAction func segementImagechange(_ sender: UISegmentedControl) {
+        
+        displayImage(index: sender.selectedSegmentIndex)
+    }
+    
+     @IBAction func slideChangeImage(_ sender: UISlider) {
+        
+        let n = Int(sender.value)
+        displayImage(index: n)
+     }
+    
+    
+    private func displayImage(index: Int)
+    {
+        var imageName = String()
+        switch index
+        {
+        case 0:
+            imageName = "lion"
+        case 1:
+            imageName = "icecream"
+        case 2:
+            imageName = "home"
+        case 3:
+            imageName = "gorilla"
+        case 4:
+            imageName = "elephant"
+        default:
+            print("No Image")
+        }
+        self.imgCircus.image = UIImage(named: imageName)
+    }
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
